94ef8ee5c30db8248e3127327e14bd4bdcc19b30
Bryan Panaccione <92956294+bryan-panaccione@users.noreply.github.com>
2021-12-26 20:57:48 -0600

    Update carouselscripts.js
    a verbose solution to part of the problem

--- ImageCarousel/js/carouselscripts.js
+++ ImageCarousel/js/carouselscripts.js
@@ -6,28 +6,58 @@ const leftButton = document.querySelector('.left')
 const rightImage = document.querySelector('#rightImg')
 const centerImage = document.querySelector('#centerImg')
 const leftImage = document.querySelector('#leftImg')
+let counter = 0
 
 //event listeners
 //rightButton.addEventListener('click', slideRight)
 //eventListener 2 - when user clicks on an item in the list -selector2- execute delCheck
 //leftButton.addEventListener("click", slideLeft)
-$(rightButton).on('click', rightButton, slideRight);
-$(leftButton).on('click', leftButton, slideLeft);
+$(rightButton).on('click', featureCounter);
+$(leftButton).on('click', featureCounter);
+$(rightButton).on('click', featureImageRB);
+$(leftButton).on('click', featureImageLB);
 
 //functions
-function slideRight() {
-  console.log('its in slideR')
-  leftImage.setAttribute('id', 'rightImg')
-  centerImage.setAttribute('id', 'leftImg')
+function featureCounter(){
+	counter +=1
+	return counter
+}
+function featureImageRB () {
+	//010 click right arrow
+	if (counter % 2 !== 0){
+		slideRight_010()
+	} else if (counter%2 ===0){
+		slideRight_100()
+	}
+}
+
+function featureImageLB () {
+	//010 click right arrow
+	if (counter % 2 !== 0){
+		slideLeft_010()
+	} else if (counter%2 ===0){
+		slideLeft_001()
+	}
+}
+
+function slideRight_010() {
+  centerImage.setAttribute('id', 'rightImg')
   rightImage.setAttribute('id', 'centerImg')
  // var rightReset = document.getElementById('rightImg');
   //var parent = rightReset.parentElement;
   //parent.insertBefore(rightReset, centerImage);
 }
+function slideRight_100() {
+  leftImage.setAttribute('id', 'leftImg')
+  centerImage.setAttribute('id', 'centerImg')
+}
+//slide function for each possible type of slide? then one major function with ifs
+function slideLeft_001() {
+  centerImage.setAttribute('id', 'centerImg')
+  rightImage.setAttribute('id', 'rightImg')
+}
 
-function slideLeft() {
-  console.log('its in slideL')
+function slideLeft_010() {
+  centerImage.setAttribute('id', 'leftImg')
   leftImage.setAttribute('id', 'centerImg')
-  centerImage.setAttribute('id', 'rightImg')
-  rightImage.setAttribute('id', 'leftImg')
 }
